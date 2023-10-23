d484ed308ac65caeb33ed5af32c770a72b54c081
Rojaon <bluerojs@gmail.com>
2023-10-23 12:21:06 +0300

    first commit

---
+++ src/test/java/com/midterm/solar/system/SolarSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midterm.solar.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
