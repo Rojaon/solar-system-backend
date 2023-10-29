6a0a57ab5c73a8384ec83763b9687ea2eccb795a
Rojaon <bluerojs@gmail.com>
2023-10-29 11:07:12 +0300

    some change

--- src/test/java/com/midterm/solar/system/repository/PlanetRepositoryTest.java
+++ src/test/java/com/midterm/solar/system/repository/PlanetRepositoryTest.java
@@ -26,17 +26,13 @@ class PlanetRepositoryTest {
 
         Planet mercury = new Planet( "Mercury",5268,new PlanetProperties(PlanetType.ROCKY_PLANET,"57292646"));
         Planet venus = new Planet("Venus",12102,new PlanetProperties(PlanetType.ROCKY_PLANET,"108147917"));
-        Planet earth = new Planet("Earth",12756,new PlanetProperties(PlanetType.ROCKY_PLANET,"149604618"));
         Planet mars = new Planet("Mars",6794,new PlanetProperties(PlanetType.ROCKY_PLANET,"229331520"));
-        Planet jupiter = new Planet("Jupiter",142984,new PlanetProperties(PlanetType.GAS_GIANT,"778278758"));
         Planet saturn = new Planet("Saturn",120536,new PlanetProperties(PlanetType.GAS_GIANT,"1421050752"));
         Planet uranus = new Planet("Uranus",51118,new PlanetProperties(PlanetType.ICE_GIANT,"2896819200"));
         Planet neptune = new Planet("Neptune",49532,new PlanetProperties(PlanetType.ICE_GIANT,"4345228800"));
         planetRepository.save(mercury);
         planetRepository.save(venus);
-        planetRepository.save(earth);
         planetRepository.save(mars);
-        planetRepository.save(jupiter);
         planetRepository.save(saturn);
         planetRepository.save(uranus);
         planetRepository.save(neptune);
@@ -47,9 +43,7 @@ class PlanetRepositoryTest {
     public void tearDown() {
         planetRepository.deleteById("Mercury");
         planetRepository.deleteById("Venus");
-        planetRepository.deleteById("Earth");
         planetRepository.deleteById("Mars");
-        planetRepository.deleteById("Jupiter");
         planetRepository.deleteById("Saturn");
         planetRepository.deleteById("Uranus");
         planetRepository.deleteById("Neptune");
@@ -58,12 +52,12 @@ class PlanetRepositoryTest {
     @Test
     public void findAll_Planets() {
         List<Planet> planetList = planetRepository.findAll();
-        assertEquals(8, planetList.size());
+        assertEquals(6, planetList.size());
     }
     @Test
     public void find_PlanetByID() {
-        Optional<Planet> planetOptional = planetRepository.findById("Earth");
-        assertEquals(12756, planetOptional.get().getDiameterInKm());
+        Optional<Planet> planetOptional = planetRepository.findById("Mercury");
+        assertEquals(5268, planetOptional.get().getDiameterInKm());
     }
     @Test
     public void find_byPropertiesDistanceFromSunInKm_correct() {
@@ -72,13 +66,13 @@ class PlanetRepositoryTest {
     }
     @Test
     public void find_byPropertiesDistanceFromSunInKm_not_correct() {
-        Optional<Planet> planetOptional = planetRepository.findByPropertiesDistanceFromSunInKm("229331520");
-        assertNotEquals("Earth", planetOptional.get().getName());
+        Optional<Planet> planetOptional = planetRepository.findByPropertiesDistanceFromSunInKm("1421050752");
+        assertNotEquals("Mars", planetOptional.get().getName());
     }
     @Test
     public void findAll_byPropertiesPlanetType_correct() {
         List<Planet> planetList = planetRepository.findByPropertiesPlanetType(PlanetType.ROCKY_PLANET);
-        assertEquals(4, planetList.size());
+        assertEquals(3, planetList.size());
     }
     @Test
     public void findAll_byPropertiesPlanetType_not_correct() {
